d0b8751d69848c7e806881e58daa78c19d1d1623
Moteen Raza <moteenraza@gmail.com>
2023-01-23 18:10:43 -0500

    added another api that shows weather for the 1 day.

---
+++ src/components/DisplayInfo.js
@@ -0,0 +1,12 @@
+
+//using props to pass down the selection of the api and displaying it on the website.
+const DisplayInfo = (props) => {
+    return (
+        <section>
+
+            <h2>{props.citySelection}</h2>                                                                                                         
+        </section>
+    )
+}
+
+export default DisplayInfo;
--- src/components/ListOfCities.js
+++ src/components/ListOfCities.js
@@ -1,4 +1,4 @@
-import CityName from './CityName';
+import DisplayInfo from './DisplayInfo';
 import CitySelection from './CitySelection';
 
 //1. import the useState and useEffect hooks from the react library
@@ -9,70 +9,82 @@ import {useState, useEffect} from 'react';
 //2. import axios
 
 import axios from 'axios';
-import { eventWrapper } from '@testing-library/user-event/dist/utils';
+
 
 const ListOfCities = () => {
 
     //3. initialize state to hold the data - cityName will be returned from the api
-    const [cityName, setCityName] = useState('');
-
-    // 
-
+    const [cityName, setCityName] = useState("");
+    const [cityKey, setCityKey] = useState("");
+    const [weatherInfo, setWeatherInfo] = useState({});
    
-    
 
+    //3b initialize another state that will hold the data for the first index from the city name array
 
 
 
 //4. define a side effect which will run ONCE on component mount to make a request to the API
 // city names
 
-useEffect(() => {
-
-
+const callCity = (city) => {
     axios(
         {
             baseURL: 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete',
                 params: {
                     apikey: '5C7XkvLTaPsO5dFsQqLGb9Qffqwg114a',
-                    q: cityName
+                    q: city
                 
             }
 
         }).then((apiData) => {
-        console.log(apiData.data);
-        // save the array of movies within State: 
-        // setCityName(apiData.data);
-        
-
-       
-    })
+            const info = apiData.data.filter(el => el.Country.ID === "CA" && el.AdministrativeArea.ID === "ON");
+            setCityKey(info[0].Key);
+           
+        })
+}
 
 
 
 
-},[cityName])
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    axios(
+        {
+            baseURL: `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${cityKey}`,
+                params: {
+                    apikey: '5C7XkvLTaPsO5dFsQqLGb9Qffqwg114a'
+                
+            }
+
+        }).then((apiData) => {
+            setWeatherInfo(apiData.data)
+           
+        })
+
+}
+
 
 // define a event handler which we be passed via props to CityName component
     // when this is called, the event handler will log out the value of the chosen city name
 
-const selectCityName = (event, chosenCityName) => {
+const handleClick = (event) => {
     event.preventDefault();
+    const city = event.target.value;
 
     
     // call the state updater function and use the selected option value to update.
-    setCityName(chosenCityName);
+    setCityName(city);
+
+    callCity(city);
 }
 
-    
-
     return (
-        <section>
-            
-            <CitySelection handleSubmit= {selectCityName}  />
-            <CityName citySelection = {cityName} />
-            
 
+        
+        <section>
+            <CitySelection handleClick={handleClick} handleSubmit={handleSubmit} cityName={cityName}/>
+            <DisplayInfo citySelection={cityName} weatherInfo = {weatherInfo} />
         </section>
 
     )
